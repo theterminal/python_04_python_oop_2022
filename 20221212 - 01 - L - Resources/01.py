48fc9758c179bc9796d1ddd08a5bf57de49c1dd7
theterminal <kirilk777@gmail.com>
2022-09-25 22:46:21 +0300

    Cleared test data

--- 20221212 - 01 - L - Resources/01.py
+++ 20221212 - 01 - L - Resources/01.py
@@ -1,16 +1,4 @@
+# 20221212 - Python -
 
 
-print('Hello me!')
 
-for i in range(3, 7):
-    print('The theme is this!')
-
-name = input()
-print(name)
-
-counter = 3
-while True:
-    print('Haha', (3 + 78 - (6 * 12)))
-    counter += 1
-    if counter == 7:
-        break
