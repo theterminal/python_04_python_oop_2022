54e4f73129f27504da5296981187022c5efc5819
theterminal <kirilk777@gmail.com>
2022-09-25 17:04:02 +0300

    Moved file

---
+++ 20221212 - 01 - L - Resources/01.py
@@ -0,0 +1,16 @@
+
+
+print('Hello me!')
+
+for i in range(3, 7):
+    print('The theme is this!')
+
+name = input()
+print(name)
+
+counter = 3
+while True:
+    print('Haha', (3 + 78 - (6 * 12)))
+    counter += 1
+    if counter == 7:
+        break
