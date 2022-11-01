481697d5e8ba51af502e2feae6d1243dac6b320b
theterminal <kirilk777@gmail.com>
2022-11-01 08:12:07 +0200

    Initial

---
+++ 20221024_03_L_First Steps in OOP/L03_Notes/L03_notes_03_keyword_nonlocal.py
@@ -0,0 +1,28 @@
+# 20221024 - Python OOP - First Steps in OOP
+# Notes 03
+
+
+print('\n__________ keyword "nonlocal" ____________\n')
+
+
+def f_1():
+    text = 'Sofia'
+    print(text, f'- From f_1()')
+
+    def f_2():
+        def f_3():
+            nonlocal text
+            text = 'Moscow'
+            print(text, '- From inside of f_3()')
+
+        f_3()
+        print(text)
+
+    f_2()
+
+
+text = 'Washington DC'
+print(text, '- Variable "text" initiated from outside of the functions')
+
+f_1()
+print(text, '- Same variable "text" initiated from outside of the functions but printed after calling of the function.')
