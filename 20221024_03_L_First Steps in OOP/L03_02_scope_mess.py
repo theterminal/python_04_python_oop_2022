5e16275bf781e414e6526737a46bf47c6135a7fb
theterminal <kirilk777@gmail.com>
2022-11-02 07:03:11 +0200

    Initial

---
+++ 20221024_03_L_First Steps in OOP/L03_02_scope_mess.py
@@ -0,0 +1,42 @@
+# 20221024 - Python OOP - First Steps in OOP
+# 02 - Scope Mess - judge url: https://judge.softuni.org/Contests/Practice/Index/1934#1
+
+
+# --------------------- version 1 --------------------- judge 100%
+
+
+x = "global"
+
+
+def outer():
+    x = "local"
+
+    def inner():
+        nonlocal x
+        x = "nonlocal"
+        print("inner:", x)
+
+    def change_global():
+        global x
+        x = "global: changed!"
+
+    print("outer:", x)
+    inner()
+    print("outer:", x)
+    change_global()
+
+
+print(x)
+outer()
+print(x)
+
+
+"""
+Output:
+------
+global
+outer: local
+inner: nonlocal
+outer: nonlocal
+global: changed!
+"""
