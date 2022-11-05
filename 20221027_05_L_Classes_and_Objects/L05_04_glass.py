f818aa06fe4911a432c059a163a459e9b98b6a29
theterminal <kirilk777@gmail.com>
2022-11-05 06:50:53 +0200

    Initial

---
+++ 20221027_05_L_Classes_and_Objects/L05_04_glass.py
@@ -0,0 +1,34 @@
+# 20221027 - Python OOP - Classes and Objects
+# 04 - Glass - judge: https://judge.softuni.org/Contests/Practice/Index/1936#3
+
+
+# --------------------- version 1 ------------------------ judge: 100%
+
+
+class Glass:
+    capacity = 250
+
+    def __init__(self):
+        self.content = 0
+
+    def fill(self, ml):
+        if Glass.capacity - self.content >= ml:
+            self.content += ml
+            return f'Glass filled with {ml} ml'
+        else:
+            return f'Cannot add {ml} ml'
+
+    def empty(self):
+        self.content = 0
+        return f'Glass is now empty'
+
+    def info(self):
+        return f'{Glass.capacity - self.content} ml left'
+
+
+glass = Glass()
+print(glass.fill(100))
+print(glass.fill(200))
+print(glass.empty())
+print(glass.fill(200))
+print(glass.info())
