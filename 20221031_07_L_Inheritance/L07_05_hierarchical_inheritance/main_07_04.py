0354a620723baaf0eca3f76609cdd719a6e2bd7c
theterminal <kirilk777@gmail.com>
2022-11-09 10:10:08 +0200

    Initial

---
+++ 20221031_07_L_Inheritance/L07_05_hierarchical_inheritance/main_07_04.py
@@ -0,0 +1,15 @@
+from project_07_05.animal import Animal
+from project_07_05.dog import Dog
+from project_07_05.cat import Cat
+
+
+a = Animal()
+print(a.eat())
+
+d = Dog()
+print(d.eat())
+print(d.bark())
+
+c = Cat()
+print(c.eat())
+print(c.meow())
