d5c079bef3f0b0208e010f380f0da1feb8b50654
theterminal <kirilk777@gmail.com>
2022-11-13 00:05:50 +0200

    Initial

---
+++ 20221103_09_L_Encapsulation/L09_Notes/L09_note_04_getter_and_setter_methods.py
@@ -0,0 +1,35 @@
+# 20221103 - Python - Python OOP - Encapsulation
+# note 04 - Getter and Setter Methods (in other languages mainly)
+
+
+"""
+Used to access and change the private variables as they are part of the class.
+Mainly setters and getters are used when data validation is needed!
+"""
+
+
+class Person:
+    def __init__(self, name, age=0):
+        self.name = name
+        self.__age = age
+
+    def info(self):
+        print(self.name)
+        print(self.__age)
+
+    def get_age(self):                  # getter
+        print(self.__age)
+
+    def set_age(self, age):             # setter
+        self.__age = age
+
+
+person = Person('Peter', 25)
+
+# accessing data using class method
+person.info()	                        # Peter
+# 		                                # 25
+
+# changing age using setter
+person.set_age(26)
+person.get_age()	                    # 26
