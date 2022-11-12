ed472caf0a31d10c8bc2722429b77efc3e76ffd8
theterminal <kirilk777@gmail.com>
2022-11-13 00:06:23 +0200

    Initial

---
+++ 20221103_09_L_Encapsulation/L09_Notes/L09_note_06_getters_and_setters.py
@@ -0,0 +1,26 @@
+# 20221103 - Python - Python OOP - Encapsulation
+# note 06 - Getters and Setters - Example
+
+
+class Car:
+    def __init__(self, max_speed: int):
+        self.max_speed = max_speed
+
+    def drive(self):
+        print('driving max speed ' + str(self.max_speed))
+
+    @property
+    def max_speed(self):
+        return self.__max_speed
+
+    @max_speed.setter
+    def max_speed(self, value):
+        if value > 447:
+            value = 447
+        self.__max_speed = value
+
+
+red_car = Car(200)
+red_car.drive()                         # driving max speed 200
+red_car.max_speed = 512                 # changes the speed to 447
+red_car.drive()                         # driving max speed 447
