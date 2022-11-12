98cecc275d0d33aa780bc40fe216d613f45174e2
theterminal <kirilk777@gmail.com>
2022-11-13 00:06:10 +0200

    Initial

---
+++ 20221103_09_L_Encapsulation/L09_Notes/L09_note_05_getters_and_setters.py
@@ -0,0 +1,33 @@
+# 20221103 - Python - Python OOP - Encapsulation
+# note 05 - Getters and Setters
+
+
+"""
+The "pythonic" way of defining getters and setters is using properties.
+By defining properties, you can change the internal implementation of a class without affecting the program.
+
+Mainly setters and getters are used when data validation is needed!
+"""
+
+
+class Person:
+    def __init__(self, age=0):
+        self.age = age
+
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, age):
+        if age < 18:
+            self.__age = 18
+        else:
+            self.__age = age
+
+
+person = Person(25)
+print(person.age)  # 25
+
+person.age = 10
+print(person.age)  # 18
