15d998926845e9ccff820e1df9cf8d23864138e1
theterminal <kirilk777@gmail.com>
2022-11-13 00:05:35 +0200

    Initial

---
+++ 20221103_09_L_Encapsulation/L09_Notes/L09_note_03_name_mangling.py
@@ -0,0 +1,24 @@
+# 20221103 - Python - Python OOP - Encapsulation
+# note 03 - Name Mangling
+
+
+"""
+Used to show that the variable should not be accessed outside the class
+
+Formula for outside access: _ClassName__MethodName
+Formula for outside access: _ClassName__AttributeName
+"""
+
+
+class Car:
+    def __init__(self):
+        self.__max_speed = 200
+
+    def drive(self):
+        print(f'driving max speed {self.__max_speed}')
+
+
+red_car = Car()
+red_car.drive()                 # driving max speed 200
+red_car.__max_speed = 10        # won't change because it is name mangled
+red_car.drive()                 # driving max speed 200
