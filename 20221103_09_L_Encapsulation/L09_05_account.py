1fcdc5389e9c3db4ebc65692152c3c1352b867e8
theterminal <kirilk777@gmail.com>
2022-11-14 08:00:51 +0200

    Initial

---
+++ 20221103_09_L_Encapsulation/L09_05_account.py
@@ -0,0 +1,28 @@
+# 20221103 - Python - Python OOP - Encapsulation
+# 05 - Email Validator - judge url: https://judge.softuni.org/Contests/Practice/Index/1938#4
+
+
+class Account:
+    def __init__(self, account_id: int, balance: int, pin: int):
+        self.__id = account_id
+        self.balance = balance
+        self.__pin = pin
+
+    def get_id(self, pin):
+        if self.__pin == pin:
+            return self.__id
+        return 'Wrong pin'
+
+    def change_pin(self, old_pin, new_pin):
+        if old_pin == self.__pin:
+            self.__pin = new_pin
+            return f'Pin changed'
+        return 'Wrong pin'
+
+
+account = Account(8827312, 100, 3421)
+print(account.get_id(1111))
+print(account.get_id(3421))
+print(account.balance)
+print(account.change_pin(2212, 4321))
+print(account.change_pin(3421, 1234))
