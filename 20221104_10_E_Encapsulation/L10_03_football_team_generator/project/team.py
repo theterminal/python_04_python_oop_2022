26dc17767bc59bf2736f7ddf9723694aeb522013
theterminal <kirilk777@gmail.com>
2022-11-15 07:55:57 +0200

    Initial

---
+++ 20221104_10_E_Encapsulation/L10_03_football_team_generator/project/team.py
@@ -0,0 +1,31 @@
+from project.player import Player
+
+
+class Team:
+    def __init__(self, name: str, rating: int):
+        self.__name = name
+        self.__rating = rating
+        self.__players = []
+
+    def add_player(self, player: Player):
+        if player in self.__players:
+            return f'Player {player.name} has already joined'
+        self.__players.append(player)
+        return f'Player {player.name} joined team {self.__name}'
+
+    def remove_player(self, player_name: str):
+        # _____________ block 1 _____________ use block 1 or block 2 _________________
+        # try:
+        #     player = next(filter(lambda x: x.name == player_name, self.__players))
+        # except StopIteration:
+        #     return f'Player {player_name} not found'
+        #
+        # self.__players.remove(player)
+        # return player
+
+        # _____________ block 2 _____________ use block 1 or block 2 _________________
+        for player in self.__players:
+            if player.name == player_name:
+                self.__players.remove(player)
+                return player
+        return f'Player {player_name} not found'
