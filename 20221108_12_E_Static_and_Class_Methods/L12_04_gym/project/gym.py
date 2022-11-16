9a42c530778d4ad334ce9fffc0a83b342dcdf498
theterminal <kirilk777@gmail.com>
2022-11-17 00:10:19 +0200

    Initial

---
+++ 20221108_12_E_Static_and_Class_Methods/L12_04_gym/project/gym.py
@@ -0,0 +1,43 @@
+from project.customer import Customer
+from project.trainer import Trainer
+from project.equipment import Equipment
+from project.exercise_plan import ExercisePlan
+from project.subscription import Subscription
+
+
+class Gym:
+    def __init__(self):
+        self.customers = []
+        self.trainers = []
+        self.equipment = []
+        self.plans = []
+        self.subscriptions = []
+
+    def add_customer(self, customer: Customer):
+        if customer not in self.customers:
+            self.customers.append(customer)
+
+    def add_trainer(self, trainer: Trainer):
+        if trainer not in self.trainers:
+            self.trainers.append(trainer)
+
+    def add_equipment(self, equipment: Equipment):
+        if equipment not in self.equipment:
+            self.equipment.append(equipment)
+
+    def add_plan(self, plan: ExercisePlan):
+        if plan not in self.plans:
+            self.plans.append(plan)
+
+    def add_subscription(self, subscription: Subscription):
+        if subscription not in self.subscriptions:
+            self.subscriptions.append(subscription)
+
+    def subscription_info(self, subscription_id: int):
+        subscription = [x for x in self.subscriptions if x.id == subscription_id][0]
+        customer = [x for x in self.customers if x.id == subscription.customer_id][0]
+        trainer = [x for x in self.trainers if x.id == subscription.trainer_id][0]
+        plan = [x for x in self.plans if x.id == subscription.exercise_id][0]
+        equipment = [x for x in self.equipment if x.id == plan.equipment_id][0]
+
+        return f'{subscription}\n{customer}\n{trainer}\n{equipment}\n{plan}'
