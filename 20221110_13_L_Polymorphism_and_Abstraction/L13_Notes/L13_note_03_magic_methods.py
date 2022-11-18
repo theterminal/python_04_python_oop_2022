b550117baa03abf77bb901a0659e7d8ef7c29936
theterminal <kirilk777@gmail.com>
2022-11-18 07:48:05 +0200

    Initial

---
+++ 20221110_13_L_Polymorphism_and_Abstraction/L13_Notes/L13_note_03_magic_methods.py
@@ -0,0 +1,40 @@
+# Magic Methods
+
+"""
+
+__add__(self, other)                        +
+__sub__(self, other)                        _
+__mul__(self, other)                        *
+__floordiv__(self, other)                   //
+__truediv__(self, other)                    /
+__pow__(self, other[, modulo])              **
+
+"""
+
+
+# ------------------ Example overloading __add__() --------------------
+
+
+"""
+If we have a class Purchase and we want to sum all expenses using the + operator, we can override the __add__ method
+"""
+
+
+class Purchase:
+    def __init__(self, product_name, cost):
+        self.product_name = product_name
+        self.cost = cost
+
+    def __add__(self, other):
+        name = f'{self.product_name}, {other.product_name}'
+        cost = self.cost + other.cost
+        return Purchase(name, cost)
+
+    def __repr__(self):
+        return f'{self.product_name}; {self.cost}'
+
+
+purchase_1 = Purchase('sofa', 650)
+purchase_2 = Purchase('table', 150)
+
+print(purchase_1 + purchase_2)                             # sofa, table; 800
