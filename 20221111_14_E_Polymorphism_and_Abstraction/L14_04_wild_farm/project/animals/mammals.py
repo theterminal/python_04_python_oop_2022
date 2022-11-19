f08ae81a4e248ef13a6fe9027a1da9b2dc325abb
theterminal <kirilk777@gmail.com>
2022-11-19 08:01:45 +0200

    Initial

---
+++ 20221111_14_E_Polymorphism_and_Abstraction/L14_04_wild_farm/project/animals/mammals.py
@@ -0,0 +1,67 @@
+from abc import ABC
+from project.animals.animal import Mammal
+from project.food import Meat, Vegetable, Fruit
+
+
+class Mouse(Mammal, ABC):
+    def __init__(self, name: str, weight: float, living_region: str):
+        super().__init__(name, weight, living_region)
+
+    @property
+    def food_that_eats(self):
+        return [Vegetable, Fruit]
+
+    @property
+    def gained_weight(self):
+        return 0.10
+
+    def make_sound(self):
+        return 'Squeak'
+
+
+class Dog(Mammal, ABC):
+    def __init__(self, name: str, weight: float, living_region: str):
+        super().__init__(name, weight, living_region)
+
+    @property
+    def food_that_eats(self):
+        return [Meat]
+
+    @property
+    def gained_weight(self):
+        return 0.40
+
+    def make_sound(self):
+        return 'Woof!'
+
+
+class Cat(Mammal, ABC):
+    def __init__(self, name: str, weight: float, living_region: str):
+        super().__init__(name, weight, living_region)
+
+    @property
+    def food_that_eats(self):
+        return [Vegetable, Meat]
+
+    @property
+    def gained_weight(self):
+        return 0.30
+
+    def make_sound(self):
+        return 'Meow'
+
+
+class Tiger(Mammal, ABC):
+    def __init__(self, name: str, weight: float, living_region: str):
+        super().__init__(name, weight, living_region)
+
+    @property
+    def food_that_eats(self):
+        return [Meat]
+
+    @property
+    def gained_weight(self):
+        return 1.00
+
+    def make_sound(self):
+        return 'ROAR!!!'
