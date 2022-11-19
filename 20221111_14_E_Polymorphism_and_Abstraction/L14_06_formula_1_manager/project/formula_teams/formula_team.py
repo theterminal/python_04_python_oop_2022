8fa76fa563ee7acaa08e233b47b920702f90e13e
theterminal <kirilk777@gmail.com>
2022-11-19 08:06:12 +0200

    Initial

---
+++ 20221111_14_E_Polymorphism_and_Abstraction/L14_06_formula_1_manager/project/formula_teams/formula_team.py
@@ -0,0 +1,39 @@
+from abc import ABC, abstractmethod
+
+
+class FormulaTeam(ABC):
+    def __init__(self, budget: int):
+        self.budget = budget
+
+    @property
+    @abstractmethod
+    def sponsors(self):
+        ...
+
+    @property
+    @abstractmethod
+    def expenses_for_one_race(self):
+        ...
+
+    @property
+    def budget(self):
+        return self.__budget
+
+    @budget.setter
+    def budget(self, value):
+        if value < 1_000_000:
+            raise ValueError('F1 is an expensive sport, find more sponsors!')
+        self.__budget = value
+
+    def calculate_revenue_after_race(self, race_pos: int):
+        revenue = 0
+
+        for positions in self.sponsors.values():            # {'Oracle': {1: 1_500_000, 2: 800_000}, 'Honda': {8: 20_000, 10: 10_000}}
+            for pos in positions:
+                if race_pos <= pos:
+                    revenue += positions[pos]
+                    break
+
+        revenue -= self.expenses_for_one_race
+        self.budget += revenue
+        return f'The revenue after the race is {revenue}$. Current budget {self.budget}$'
