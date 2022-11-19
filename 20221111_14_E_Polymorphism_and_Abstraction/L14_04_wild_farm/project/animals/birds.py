9b245c91474420ec8424d215282837f272ec86b9
theterminal <kirilk777@gmail.com>
2022-11-19 08:01:29 +0200

    Initial

---
+++ 20221111_14_E_Polymorphism_and_Abstraction/L14_04_wild_farm/project/animals/birds.py
@@ -0,0 +1,35 @@
+from abc import ABC
+from project.animals.animal import Bird
+from project.food import Vegetable, Fruit, Seed, Meat
+
+
+class Owl(Bird):
+    def __init__(self, name: str, weight: float, wing_size: float):
+        super().__init__(name, weight, wing_size)
+
+    @property
+    def food_that_eats(self):
+        return [Meat]
+
+    @property
+    def gained_weight(self):
+        return 0.25
+
+    def make_sound(self):
+        return 'Hoot Hoot'
+
+
+class Hen(Bird, ABC):
+    def __init__(self, name: str, weight: float, wing_size: float):
+        super().__init__(name, weight, wing_size)
+
+    @property
+    def food_that_eats(self):
+        return [Vegetable, Fruit, Seed, Meat]
+
+    @property
+    def gained_weight(self):
+        return 0.35
+
+    def make_sound(self):
+        return 'Cluck'
