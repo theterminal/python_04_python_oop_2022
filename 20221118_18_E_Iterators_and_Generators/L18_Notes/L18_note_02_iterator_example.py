f9d73220513f9ca241dc2190a2a7005feae85d89
theterminal <kirilk777@gmail.com>
2022-11-25 08:34:16 +0200

    Initial

---
+++ 20221118_18_E_Iterators_and_Generators/L18_Notes/L18_note_02_iterator_example.py
@@ -0,0 +1,21 @@
+# 20221118 - Python - Python OOP - Iterators and Generators
+# Note 02 - Iterator
+
+
+class countdown_iterator:
+    def __init__(self, count: int):
+        self.count = count
+
+    def __iter__(self):
+        return iter('abc')                         # could use: iter(['abc', 'def']), iter({'abc', 'def'})
+
+    def __next__(self):
+        if self.count < 0:
+            raise StopIteration
+        self.count -= 1
+        return self.count + 1
+
+
+iterator = countdown_iterator(10)
+for item in iterator:
+    print(item, end=" ")                           # a b c
