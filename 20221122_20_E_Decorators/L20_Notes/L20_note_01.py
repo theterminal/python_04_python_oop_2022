632a862fac072dc84326d1dd58fbb7e5141375c7
theterminal <kirilk777@gmail.com>
2022-11-28 11:44:02 +0200

    Initial

---
+++ 20221122_20_E_Decorators/L20_Notes/L20_note_01.py
@@ -0,0 +1,12 @@
+# 20221122 - Python - Python OOP - Decorators
+# Note 01 - Execution Time
+
+
+import time
+
+
+start = time.time()
+time.sleep(3)
+end = time.time()
+
+print(f'The time between start and end is {end - start} seconds!')
