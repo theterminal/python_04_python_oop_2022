750c5f42ee6d7c76929e88010053b01a7b143473
theterminal <kirilk777@gmail.com>
2022-12-08 15:35:26 +0200

    Initial

---
+++ 20221024_03_L_First Steps in OOP/L03_Notes/L03_note_07_creating_a_class_with_one_method.py
@@ -0,0 +1,17 @@
+# 20221024 - Python OOP - First Steps in OOP
+# Note 07 - Creating a class with one method
+
+
+class Animal:
+    def __init__(self, name: str):                  # initializing method
+        self.name = name
+
+    def is_asleep(self):                            # method (defines an action for the object)
+        return f'{self.name} is sleeping!'
+
+
+animal_1 = Animal('The Big Bear')
+print(animal_1.is_asleep())                         # The Big Bear is sleeping!
+
+animal_2 = Animal('The Pink Panther')
+print(animal_2.is_asleep())                         # The Pink Panther is sleeping!
