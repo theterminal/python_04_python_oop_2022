be27fc50863998f1f383eb09f483cd9ab888dc5c
theterminal <kirilk777@gmail.com>
2022-12-08 18:18:40 +0200

    Initial

--- 20221027_05_L_Classes_and_Objects/L05_Notes/L05_note_01_method__str__.py
+++ 20221027_05_L_Classes_and_Objects/L05_Notes/L05_note_01_method__str__.py
@@ -1,5 +1,5 @@
 # 20221027 - Python OOP - Classes and Objects
-# Methods __str__, __repr__
+# Note 1 - Method __str__
 
 
 print('\n---------- print without __str__ method ------------------\n')
@@ -13,9 +13,6 @@ class Car:
     def change_year(self, new_year):
         self.year = new_year
 
-    # def __str__(self):
-    #     return f'{self.model} made {self.year}'
-
 
 nissan = Car('GT-R', 2012)
 toyota = Car('Supra', 1999)
@@ -23,7 +20,7 @@ toyota = Car('Supra', 1999)
 cars = [nissan, toyota]
 
 for car in cars:
-    print(car)
+    print(car)                                  # prints out the object and the address of residence
 
 
 print('\n---------- print with __str__ method ------------------\n')
@@ -37,7 +34,7 @@ class Car:
     def change_year(self, new_year):
         self.year = new_year
 
-    def __str__(self):                                      # changed from above
+    def __str__(self):
         return f'{self.model} made {self.year}'
 
 
@@ -50,31 +47,3 @@ for car in cars:
     print(car)                                              # any of the 3 lines work
     print(str(car))
     print(car.__str__())
-
-
-print('\n---------- print with __repr__ method (easier when in debug mode) ------------------\n')
-
-
-class Car:
-    def __init__(self, model, year):
-        self.model = model
-        self.year = year
-
-    def change_year(self, new_year):
-        self.year = new_year
-
-    def __repr__(self):     # changed from __str__ to __repr__ {When debug it shows this string type and not object}
-        return f'{self.model} made {self.year}'
-
-
-nissan = Car('GT-R', 2012)
-toyota = Car('Supra', 1999)
-
-cars = [nissan, toyota]
-
-for car in cars:
-    print(car)                                              # any of the 4 lines work
-    print(str(car))
-    print(car.__str__())
-    print(car.__repr__())
-
