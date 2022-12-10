19762108832993c879cbf7ba36bcdd9c47a9d8f4
theterminal <kirilk777@gmail.com>
2022-12-10 10:01:22 +0200

    Refactored

--- 20221103_09_L_Encapsulation/L09_Notes/L09_note_09_getattr()_and__getattr__().py
+++ 20221103_09_L_Encapsulation/L09_Notes/L09_note_09_getattr()_and__getattr__().py
@@ -1,16 +1,18 @@
 # 20221103 - Python - Python OOP - Encapsulation
-# note 09 - Function getattr() and method __getattr()__
+# Note 09 - Function getattr() and method __getattr()__
 
 
 print('\n________________ function getattr() ___________________\n')
 
 
 """
+
 getattr()
 Used to access the attribute of an object.
 Returns the value of the named attribute .
 
 The method takes multiple parameters: 'Object', 'Name', 'Default' (optional)
+
 """
 
 
@@ -29,51 +31,50 @@ print('\n________________ method __getattr__() _____ ex. 1 ______\n')
 
 
 """
+
 Called when an attribute lookup has not found the attribute in the usual places.
 The method takes 1 parameter – the 'Name' of the attribute
 
 When accessing 'phone.color', Python calls "phone.__getattr__('color')"
+
 """
 
 
-# class Phone:
-#     def __getattr__(self, attr):
-#         return None
-#
-#
-# phone = Phone()
-# print(phone.color)                              # None
-# print(getattr(phone, 'size'))                   # None
+class Phone:
+    def __getattr__(self, attr):
+        return None
 
 
-print('\n------------------------ ex. 2 ------------------------\n')
+phone = Phone()
+print(phone.color)                              # None
+print(getattr(phone, 'size'))                   # None
 
 
-# class Phone:
-#     def __getattr__(self, attr):
-#         return attr
-#
-#
-# phone = Phone()
-# print(phone.color)                              # color
-# print(getattr(phone, 'size'))                   # size
+print('\n________________ method __getattr__() _____ ex. 2 ______\n')
 
 
-print('\n------------------------ ex. 3 ------------------------\n')
+class Phone:
+    def __getattr__(self, attr):
+        return attr
 
 
-# class Phone:
-#     def __init__(self, color: str, size: int):
-#         self.color = color
-#         self.size = size
-#
-#     def __getattr__(self, attr):
-#         return None
-#
-#
-# phone = Phone('blue', 12)
-# print(phone.color)                              # None only if attr is missing
-# print(getattr(phone, 'size'))                   # None only if attr is missing
+phone = Phone()
+print(phone.color)                              # color
+print(getattr(phone, 'size'))                   # size
 
 
-print('\n------------------------------------------------------\n')
+print('\n________________ method __getattr__() _____ ex. 3 ______\n')
+
+
+class Phone:
+    def __init__(self, color: str, size: int):
+        self.color = color
+        self.size = size
+
+    def __getattr__(self, attr):
+        return None
+
+
+phone = Phone('blue', 12)
+print(phone.color)                              # None only if attr is missing
+print(getattr(phone, 'size'))                   # None only if attr is missing
