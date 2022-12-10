3e01033aea1816c1df1156112cf796d3a2eba9ef
theterminal <kirilk777@gmail.com>
2022-12-10 10:03:46 +0200

    Refactored

--- 20221107_11_L_Static_and_Class_Methods/L11_Notes/L11_note_01_static_methods.py
+++ 20221107_11_L_Static_and_Class_Methods/L11_Notes/L11_note_01_static_methods.py
@@ -3,19 +3,21 @@
 
 
 """
+
 It knows nothing about the class or instance it is called on.
 It cannot modify object state or class state.
 It could be put outside the class, but it is inside the class where it is applicable.
-To turn a method into a static, we add a line with @staticmethod in front of the method header.
+To turn a method into a static, a line with @staticmethod in front of the method header must be added.
 
-To call a static method, we could use both the instance, or the class
+To call a static method, both, the instance or the class can be used.
 
 Benefits:
 --------
-Shows that a particular method is independent from everything else around it.
+Shows that a particular method is independent of everything else around it.
 Often helps to avoid accidentals modifications that go against the original design.
 Communicates and enforces developer intent about the class design.
-It is much easier to test since it is completely independent from the rest of the class.
+It is much easier to test since it is completely independent of the rest of the class.
+
 """
 
 
