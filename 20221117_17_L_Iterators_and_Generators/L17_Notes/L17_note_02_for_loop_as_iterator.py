3eccb89d2f2671e7b1d66e1c338d6cdbf1883ba8
theterminal <kirilk777@gmail.com>
2022-12-11 17:17:21 +0200

    Refactored

--- 20221117_17_L_Iterators_and_Generators/L17_Notes/L17_note_02_for_loop_as_iterator.py
+++ 20221117_17_L_Iterators_and_Generators/L17_Notes/L17_note_02_for_loop_as_iterator.py
@@ -3,15 +3,17 @@
 
 
 """
+
 The for loop can iterate automatically through the list
 The for loop can iterate over any iterable
 A for loop is implemented as:
+
 """
 
 iter_obj = iter(iterable)
 while True:
     try:
-        element = next(iter_obj)            # get the next item
+        element = next(iter_obj)                                # get the next item
         # do something with element
     except StopIteration:
         # if StopIteration is raised, break from loop
@@ -19,7 +21,9 @@ while True:
 
 
 """
+
 The for loop creates an iterator object (iter_obj) by calling iter() on the iterable
 Inside the loop, it calls next() to get the next element and executes the body of the for loop with this value
 After all the items exhaust, StopIteration is raised, which is internally caught, and the loop ends
+
 """
