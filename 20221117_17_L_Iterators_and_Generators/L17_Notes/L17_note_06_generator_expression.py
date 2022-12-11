cc13a63b62087f83d05239b8b4dabecef0f731aa
theterminal <kirilk777@gmail.com>
2022-12-11 17:17:35 +0200

    Refactored

--- 20221117_17_L_Iterators_and_Generators/L17_Notes/L17_note_06_generator_expression.py
+++ 20221117_17_L_Iterators_and_Generators/L17_Notes/L17_note_06_generator_expression.py
@@ -1,12 +1,14 @@
 # 20221117 - Python - Python OOP - Iterators and Generators
-# Note 04 - Generator Expression
+# Note 06 - Generator Expression
 
 
 """
+
 Generators can be easily created using generator expressions.
 Same as lambda function creates an anonymous function, generator expression creates an anonymous generator function.
 The syntax for generator expression is similar to that of a list comprehension.
 The difference between them is that generator expression produces one item at a time.
+
 """
 
 
