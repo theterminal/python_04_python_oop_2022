237e2e849de96a903ed539ea5193fb9bd36cad83
theterminal <kirilk777@gmail.com>
2022-12-12 07:08:10 +0200

    Refactored

--- 20221121_19_L_Decorators/L19_Notes/L19_note_07_decorating_methods_in_classes.py
+++ 20221121_19_L_Decorators/L19_Notes/L19_note_07_decorating_methods_in_classes.py
@@ -3,10 +3,12 @@
 
 
 """
+
 @classmethod            - decorator function that converts a method to a class method
 @abstractmethod         - decorator function that converts an instance method to an abstract instance method
 @abstractclassmethod    - decorator function that converts a class method to an abstract class method
 @property               - change your class methods/attributes so that the user of a class doesn't need to make any change in their code
+
 """
 
 
