9e501c2a35e44d2bba50f266f949bc1c6aba5664
theterminal <kirilk777@gmail.com>
2022-12-12 07:13:23 +0200

    Initial

---
+++ 20221210_28_Regular_Exam/Problem_1_and_2/project/booths/private_booth.py
@@ -0,0 +1,10 @@
+from project.booths.booth import Booth
+
+
+class PrivateBooth(Booth):                                                     # 1, 2, 3, 4, 5, 6                 42/50
+    def __init__(self, booth_number: int, capacity: int):
+        super().__init__(booth_number, capacity)
+
+    def reserve(self, number_of_people: int):
+        self.price_for_reservation = number_of_people * 3.50
+        self.is_reserved = True                                             # check later
