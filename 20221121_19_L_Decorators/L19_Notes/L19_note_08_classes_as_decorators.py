8246ce971b9675d038ad98597552b4a949a77865
theterminal <kirilk777@gmail.com>
2022-12-12 07:08:16 +0200

    Refactored

--- 20221121_19_L_Decorators/L19_Notes/L19_note_08_classes_as_decorators.py
+++ 20221121_19_L_Decorators/L19_Notes/L19_note_08_classes_as_decorators.py
@@ -3,10 +3,12 @@
 
 
 """
+
 We can also use classes as decorators.
 We usually do that when we need to maintain a state.
 To use a class as a decorator, we need to implement the __call__ method.
 The __call__ method allows class instances to be called as functions.
+
 """
 
 
