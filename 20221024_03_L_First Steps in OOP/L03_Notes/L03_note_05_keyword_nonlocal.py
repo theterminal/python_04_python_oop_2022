2de538bf8d8abaf29b9bae474fca27d756468fbd
theterminal <kirilk777@gmail.com>
2022-12-13 12:39:43 +0200

    Refactored

--- 20221024_03_L_First Steps in OOP/L03_Notes/L03_note_05_keyword_nonlocal.py
+++ 20221024_03_L_First Steps in OOP/L03_Notes/L03_note_05_keyword_nonlocal.py
@@ -7,6 +7,7 @@ def f_1():
     print(text, f'- From f_1()')
 
     def f_2():
+        print(text, '- From inside f_2(), as first line after the def statement.')
 
         def f_3():
             nonlocal text
@@ -14,7 +15,7 @@ def f_1():
             print(text, '- From inside of f_3()')
 
         f_3()
-        print(text)
+        print(text, '- From inside of f_2(), after definition of f_3() and statement nonlocal inside f_3(), that actually changed the variable')
 
     f_2()
 
@@ -24,3 +25,13 @@ print(text, '- Variable "text" initiated from outside of the functions')
 
 f_1()
 print(text, '- Same variable "text" initiated from outside of the functions but printed after calling of the function.')
+
+
+"""
+
+
+The variable 'text' declared outside the functions is different from the variable 'text' from the inside the functions.
+The variable 'text' declared inside f_1() is the same variable inside f_2() and f_3(). 
+
+
+"""
