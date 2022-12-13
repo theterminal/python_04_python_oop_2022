dcea9b9e3edb03899d8236a87157c25873ffd1ea
theterminal <kirilk777@gmail.com>
2022-12-13 12:39:56 +0200

    Refactored

--- 20221024_03_L_First Steps in OOP/L03_Notes/L03_z01_figures_of_stars.py
+++ 20221024_03_L_First Steps in OOP/L03_Notes/L03_z01_figures_of_stars.py
@@ -5,20 +5,23 @@
 print('\n--------------------- ex. 1 ------------------------\n')
 
 
-n = int(input())
+n = int(input("Enter an integer, representing the number of rows you want the triangle to have: "))
 
 for row in range(1, n + 1):
     print(' ' * (n - row), end='')
+
     for col in range(1, row + 1):
         print('* ', end='')
     print()
 
-'''
+
+"""
 
 
 input:
 ------
 5
+
 
 result:
 ------
@@ -29,26 +32,29 @@ result:
 * * * * * 
 
 
-'''
+"""
 
 
 print('\n--------------------- ex. 2 ------------------------\n')
 
 
-n = int(input())
+n = int(input("Enter an integer, representing the number of rows you want the trapezoid to have: "))
 
 for row in range(1, n + 1):
     print(' ' * (n - row), end='')
+
     for col in range(1, row + 2):
         print('* ', end='')
     print()
 
-'''
+
+"""
 
 
 input:
 ------
 5
+
 
 result:
 ------
@@ -59,26 +65,29 @@ result:
 * * * * * * 
 
 
-'''
+"""
 
 
 print('\n--------------------- ex. 3 ------------------------\n')
 
 
-n = int(input())
+n = int(input("Enter an integer, representing the number of rows you want the triangle to have: "))
 
 for row in range(1, n + 1):
     print(' ' * (n - row + 10), end='')
+
     for col in range(1, row + 1):
         print('* ', end='')
     print()
 
-'''
+
+"""
 
 
 input:
 ------
 5
+
 
 result:
 ------
@@ -89,13 +98,13 @@ result:
           * * * * * 
 
 
-'''
+"""
 
 
 print('\n--------------------- ex. 4 ------------------------\n')
 
 
-n = int(input())
+n = int(input("Enter an integer, representing the number of columns you want the longest row of the triangle to have: "))
 
 for row in range(1, n + 1):
     print('* ' * row)
@@ -104,12 +113,13 @@ for row in range(n - 1, -1, -1):
     print('* ' * row)
 
 
-'''
+"""
 
 
 input:
 ------
 5
+
 
 result:
 ------
@@ -124,45 +134,49 @@ result:
 * 
 
 
-'''
+"""
 
 
 print('\n--------------------- ex. 5 ------------------------\n')
 
 
-n = int(input())
+n = int(input("Enter an ODD integer, representing the number of rows you want the diamond to have: "))
+n = int(n / 2) + 1
 
 for row in range(1, n + 1):
     print(' ' * (n - row), end='')
+
     for col in range(1, row + 1):
         print('* ', end='')
     print()
 
 for row in range(n - 1, -1, -1):
     print(' ' * (n - row), end='')
+
     for col in range(1, row + 1):
         print('* ', end='')
     print()
 
 
-'''
+"""
+
+
+If you enter ODD number everything will work correct.
+If you enter EVEN number you will get a diamond with number of rows - next ODD number after the EVEN number you entered.
 
 
 input:
 ------
 5
 
+
 result:
 ------
-    * 
-   * * 
-  * * * 
- * * * * 
-* * * * * 
- * * * * 
-  * * * 
-   * * 
-    * 
+  * 
+ * * 
+* * * 
+ * * 
+  * 
 
 
-'''
+"""
