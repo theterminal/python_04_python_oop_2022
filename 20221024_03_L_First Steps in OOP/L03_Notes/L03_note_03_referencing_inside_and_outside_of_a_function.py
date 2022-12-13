4171e25baed4c5011982dacfb259f9d74702f1af
theterminal <kirilk777@gmail.com>
2022-12-13 12:39:31 +0200

    Refactored

--- 20221024_03_L_First Steps in OOP/L03_Notes/L03_note_03_referencing_inside_and_outside_of_a_function.py
+++ 20221024_03_L_First Steps in OOP/L03_Notes/L03_note_03_referencing_inside_and_outside_of_a_function.py
@@ -14,10 +14,15 @@ print(elements)
 
 '''
 
-As it is visible from the result, the referencing type of data could be changed from inside of a function without
-being specifically given as an argument to the function. Same is not possible with a non-referencing data type,
-number for example. In other words the reference to the data cannot be changed from inside a function without data 
-being imported as an argument first. The appending of the list in the example is possible due to the same reason.
+As it is visible from the result, the REFERENCING type of data CAN be changed from inside of a function without
+being specifically given as an argument to the function.
+
+Same is NOT possible with a NON-REFERENCING data type, number for example.
+
+In other words the reference to the data cannot be changed from inside a function without data 
+being imported as an argument first!
+
+The appending of the list in the example is possible due to the same reason.
 We are not assigning new data carrier but we are adding to the already existing data, just referencing it.   
 
 '''
